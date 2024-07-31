8f37889907932d47ee85785d9f26e59deed05cfe
malek <malekmohamed234@gmail.com>
2024-07-31 13:20:05 +0300

    Revert "Added minor changes in style"
    This reverts commit c3ae370538d3f42eb068ffabffd406ae07550a0f.

--- app/src/main/java/com/example/carbooking/main/MainActivity.kt
+++ app/src/main/java/com/example/carbooking/main/MainActivity.kt
@@ -1,186 +1,49 @@
 package com.example.carbooking.main
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.lifecycleScope
-import androidx.viewpager.widget.ViewPager
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
 import com.example.carbooking.MarsApi.retrofitService
 import com.example.carbooking.R
-import com.example.carbooking.RecyclerViewHome
-import com.example.carbooking.fragments.LoginFragment
-import com.example.carbooking.fragments.RegisterFragment
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.example.carbooking.authentication.Login
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var view_pager: ViewPager2
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
+    lateinit var textView: TextView
 
-    companion object {
-        private const val TAG = "GoogleActivity"
-        private const val RC_SIGN_IN = 9001
-    }
-
-    // [START on_start_check_user]
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
-    }
-    // [END on_start_check_user]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        window.statusBarColor = Color.WHITE
 
-        val loginFragment = LoginFragment()
-        val registerFragment = RegisterFragment()
+        val log_out: Button = findViewById(R.id.log_out)
 
-        view_pager = findViewById(R.id.view_pager)
-        val screen_container: ConstraintLayout = findViewById(R.id.screen_container)
-        val tab_layout: TabLayout = findViewById(R.id.tab_layout)
-        val google_button: Button = findViewById(R.id.google)
-        val facebook_button: Button = findViewById(R.id.facebook)
+        textView = findViewById(R.id.textView)
 
-        // [START config_signin]
-        // Configure Google Sign In
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail().build()
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
-        // [END config_signin]
-
-        val sharedPrefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val isSignedIn = sharedPrefs.getBoolean("isSignedIn", false)
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-
-        view_pager.adapter = ViewPagerAdapter(this)
-        TabLayoutMediator(tab_layout, view_pager){ tab, position ->
-            tab.text = if(position == 0) "Login" else "Register"
-        }.attach()
-
-        google_button.setOnClickListener {
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-            // Get the current user
-            val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
-
-            // Get the ID token
-            user?.getIdToken(true)?.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val idToken: String? = task.result?.token
-                    val credential = GoogleAuthProvider.getCredential(idToken, null)
-                    val editor = sharedPrefs.edit()
-                    editor.putBoolean("isSignedIn", true)
-                    editor.apply()
-                    startActivity(Intent(this, RecyclerViewHome::class.java))
-                    val user = auth.currentUser
-                    //updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "Google sign in failed", task.exception)
-                    updateUI(null)
-                }
-            }
+        log_out.setOnClickListener {
+            Firebase.auth.signOut()
+            startActivity(Intent(this, Login::class.java))
         }
+
+        getMarsPhotos()
     }
 
-    // [END auth_with_google]
-
-    // [START onactivityresult]
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+    private fun getMarsPhotos() {
+        lifecycleScope.launch {
             try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                //Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-            }
-        }
-    }
-    // [END onactivityresult]
+                val listResult = retrofitService.getPhotos()
+                textView.text = "Success. ${listResult.size} Mars photos retrieved"
+            } catch (e: IOException) {
 
-    // [START auth_with_google]
-    private fun firebaseAuthWithGoogle(idToken: String?) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-//                    displayNotification(pendingIntent =
-//                    PendingIntent.getActivity(this, 0, intent,
-//                        PendingIntent.FLAG_IMMUTABLE))
-                    val user = auth.currentUser
-                    Log.d(TAG, "signInWithCredential:success: ${user?.displayName}")
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "Google sign in failed", task.exception)
-                    updateUI(null)
-                }
-            }
-    }
-    // [END auth_with_google]
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-    // [END signin]
-
-    private fun updateUI(user: FirebaseUser?) {
-
-    }
-
-    class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
-        override fun getItemCount(): Int = 2
-
-        override fun createFragment(position: Int): Fragment {
-            return when (position){
-                0 -> LoginFragment()
-                else -> RegisterFragment()
             }
         }
     }
